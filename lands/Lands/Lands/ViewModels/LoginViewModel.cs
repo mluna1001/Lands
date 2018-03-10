f3524e06d90a4b1a5a9cd0f20b43d30be6831a44
Marco Antonio Luna Serrano <mluna1001@gmail.com>
2018-03-10 17:37:39 -0600

    LoginViewModel de forma apropiada
    A wevo, quedó más genérico el cambio de propiedades haciendo un ViewModel base para todos.

--- lands/Lands/Lands/ViewModels/LoginViewModel.cs
+++ lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,17 +1,42 @@
 ﻿namespace Lands.ViewModels
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        public string Email
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get { return password; }
+            set { SetValue(ref password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+        public bool IsRemembered
+        {
+            get; set;
+        }
+        public bool IsEnabled
+        {
+            get { return isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
@@ -19,11 +44,62 @@
         {
             this.IsRemembered = true;
             this.IsRunning = false;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept");
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "mluna1001@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck yeah!!",
+                    "Accept");
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+        }
         #endregion
     }
 }
