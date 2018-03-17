9f601b33309f26b51b6d577ab48be4d465854117
Marco Antonio Luna Serrano <mluna1001@gmail.com>
2018-03-17 16:11:40 -0600

    LandsViewModel
    Obtiene Lands de la API

---
+++ lands/Lands/Lands/Models/RegionalBloc.cs
@@ -0,0 +1,20 @@
+﻿namespace Lands.Models
+{
+    using Newtonsoft.Json;
+    using System.Collections.Generic;
+
+    public class RegionalBloc
+    {
+        [JsonProperty(PropertyName = "acronym")]
+        public string Acronym { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        //[JsonProperty(PropertyName = "otherAcronyms")]
+        //public List<object> OtherAcronyms { get; set; }
+
+        //[JsonProperty(PropertyName = "otherNames")]
+        //public List<object> OtherNames { get; set; }
+    }
+}
