86999dcd9c81638c78fefd1457b1765941fc7a81
Marco Antonio Luna Serrano <mluna1001@gmail.com>
2018-03-26 06:54:45 -0600

    Login with Facebook
    Implementación API Login con Facebook, falta implementar en movil

---
+++ lands/Lands/Lands/Views/LoginFacebookPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Lands.Views
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginFacebookPage : ContentPage
+	{
+		public LoginFacebookPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
